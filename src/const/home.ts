ccf5662c73a8df38b0a736b47c09cff7f54f5195
吴大卫 <wudaweia@enn.cn>
2022-07-12 14:57:58 +0800

    feat: add api path

--- src/const/home.ts
+++ src/const/home.ts
@@ -1,3 +1,5 @@
 const API_ENN_RBAC_AUTH = "/api/enn-rbac-auth";
+const API_ENN_USER = "/api/enn-user";
+const API_ENN_SYSTEM = "/api/enn-system";
 
-export { API_ENN_RBAC_AUTH };
+export { API_ENN_RBAC_AUTH, API_ENN_USER, API_ENN_SYSTEM };
