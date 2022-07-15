dbb34ae11a081dd2602502be0039c6b386b7d2a0
吴大卫 <wudaweia@enn.cn>
2022-07-15 13:48:44 +0800

    feat: fix menu

--- src/views/layout/menu.ts
+++ src/views/layout/menu.ts
@@ -1,27 +1,26 @@
 import { MenuInfo } from "@/types";
 
 const menuList: MenuInfo[] = [
+  // {
+  //   name: "首页",
+  //   desc: "首页",
+  //   key: "home",
+  //   url: "/home",
+  //   icon: "",
+  // },
   {
-    name: "首页",
-    desc: "首页",
-    key: "home",
-    url: "/home",
-    icon: "",
-  },
-  {
-<<<<<<< HEAD
     name: "成员管理",
     desc: "成员管理",
     key: "member",
     url: "/member",
     icon: "",
-=======
+  },
+  {
     name: "组织管理",
     desc: "组织管理",
     key: "organManage",
     url: "/organManage",
-    icon: ''
->>>>>>> dev-0711-cqp
+    icon: "",
   },
 ];
 export default menuList;
