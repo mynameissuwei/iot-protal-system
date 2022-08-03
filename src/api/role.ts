af30d8d9f6bcd10757005f232bfc60611ad7faf2
liuqq <liuqq@enn.cn>
2022-08-03 10:11:43 +0800

    代码恢复和优化

--- src/api/role.ts
+++ src/api/role.ts
@@ -57,3 +57,21 @@ export function getRoleListMenu(params: any): Promise<any> {
     params,
   });
 }
+
+// 获取角色权限
+export function getRoletList(params: any): Promise<any> {
+  return Service({
+    method: "get",
+    url: `${API_ENN_SYSTEM}/role/list/all`,
+    params,
+  });
+}
+
+//角色增加权限
+export function grantRoles(data: any): Promise<any> {
+  return Service({
+    method: "post",
+    url: `${API_ENN_SYSTEM}/role/grant`,
+    data,
+  });
+}
