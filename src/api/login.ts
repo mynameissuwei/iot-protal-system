e7fa37e144841dc8fc201b1e9840db780fcd5939
吴大卫 <wudaweia@enn.cn>
2022-09-13 15:27:06 +0800

    feat: fix package

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,10 +1,10 @@
 import Service from "@/axios";
 import { API_ENN_RBAC_AUTH } from "@/const";
-// import { cipherMode, sm2PublicKey } from "@/utils/sm2Key.js";
-// import { sm2 } from "sm-crypto";
+import { cipherMode, sm2PublicKey } from "@/utils/sm2Key";
+import { sm2 } from "sm-crypto";
 
 //是否登录
-export function isLoginApi(data: any): Promise<any> {
+export const isLoginApi = (data: any): any => {
   return Service({
     url: `${API_ENN_RBAC_AUTH}/token/check`,
     method: "get",
@@ -12,13 +12,71 @@ export function isLoginApi(data: any): Promise<any> {
       accessToken: data,
     },
   });
-}
+};
 
 // 退出登录
-export const logout = (data: any): any => {
-  console.log(data);
+export const logout = (): any => {
   return Service({
     url: `${API_ENN_RBAC_AUTH}/logout`,
     method: "get",
   });
 };
+
+//  LDAP
+export const ldapCheck = (): any => {
+  return Service({
+    url: `${API_ENN_RBAC_AUTH}/ldap/check`,
+    method: "post",
+  });
+};
+
+export const loginBase = (): any => {
+  return Service({
+    url: `${API_ENN_RBAC_AUTH}/captcha`,
+    method: "get",
+  });
+};
+
+export const loginApi = (params: any): any => {
+  const { account, password, ...res } = params;
+  const data = {
+    account: "04" + sm2.doEncrypt(account, sm2PublicKey, cipherMode),
+    password: "04" + sm2.doEncrypt(password, sm2PublicKey, cipherMode),
+    ...res,
+  };
+  return Service({
+    url: `${API_ENN_RBAC_AUTH}/token`,
+    method: "post",
+    data,
+  });
+};
+
+// 登录时获取短信验证码
+export const loginCodeApi = (data: any): any => {
+  return Service({
+    url: `${API_ENN_RBAC_AUTH}/captcha/login/sms?mobile=${data}`,
+    method: "get",
+  });
+};
+
+// 获取手机短信验证码后登录 3   需改造 合并成一个接口了
+export const codeLoginApi = (data: any): any => {
+  return Service({
+    url: `${API_ENN_RBAC_AUTH}/token/sms?mobile=${data.mobile}&smsCaptcha=${data.smsCaptcha}`,
+    method: "post",
+  });
+};
+
+// //  注册接口
+// export const loginUp = (data: any): any => {
+//   return Service({
+//     url: "/api/enn-user/user/register/submit",
+//     method: "post",
+//     data,
+//     headers: {
+//       "Content-Type": "application/json;charset=UTF-8",
+//       "Captcha-Key": data.CaptchaKey,
+//       "Captcha-Code": data.CaptchaCode,
+//     },
+//   });
+// };
